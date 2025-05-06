876a81725c116bb2b6e27c313c0da3f2c7144534
Yanomafpk <yanoma.konwski@sou.inteli.edu.br>
2025-05-06 10:30:01 -0300

    add

---
+++ YanomaFenandesKonwski/26.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,4,1,2,3,4]
+
+def remove(nums):
+    ocorrencias={}
+    total=0
+    i=0
+    for numero in nums:
+        if (numero in ocorrencias):
+            ocorrencias[numero] +=1
+        else:
+            ocorrencias[numero] = 1
+    
+    for numero in ocorrencias:
+        nums[i] = numero
+        total+=1
+        i+=1
+    return total
+
+print(remove(nums))
