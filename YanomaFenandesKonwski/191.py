859a0e439555dbc82aeb776fee681e5b1610991f
Yanomafpk <yanoma.konwski@sou.inteli.edu.br>
2025-05-11 23:05:08 -0300

    vamo

--- YanomaFenandesKonwski/191.py
+++ YanomaFenandesKonwski/191.py
@@ -1,13 +1,10 @@
-n = 11
-auxiliar = 0
-dicionario = {0:0, 1:0}
-while n>=1:
-    if n%2 == 1:
-        dicionario[1] +=1
-    else:
-        dicionario[0] += 1
-    n = int(n/2)
-
-print (dicionario[1])
-
-
+class Solution(object):
+    def hammingWeight(self, n):
+        dicionario = {0:0, 1:0}
+        while n>=1:
+            if n%2 == 1:
+                dicionario[1] +=1
+            else:
+                dicionario[0] += 1
+            n = int(n/2)
+        return dicionario[1]
