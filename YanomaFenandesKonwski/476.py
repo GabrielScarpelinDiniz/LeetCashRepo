99cfc6ef2c7827e7046650d44449cb266090e423
Yanomafpk <yanoma.konwski@sou.inteli.edu.br>
2025-05-23 01:35:09 -0300

    .

--- YanomaFenandesKonwski/476.py
+++ YanomaFenandesKonwski/476.py
@@ -7,17 +7,17 @@ class Solution(object):
             bin_str = str(resto) + bin_str
             n = n // 2
 
-        inverted_str = ""
+        inverte = ""
         for caractere in bin_str:
             if caractere == '1':
-                inverted_str += '0'
+                inverte += '0'
             else:
-                inverted_str += '1'
+                inverte += '1'
 
         resultado = 0
         expoente = 0
-        for i in range(len(inverted_str) - 1, -1, -1):
-            bit = int(inverted_str[i])
+        for i in range(len(inverte) - 1, -1, -1):
+            bit = int(inverte[i])
             resultado += bit * (2 ** expoente)
             expoente += 1
         return resultado
