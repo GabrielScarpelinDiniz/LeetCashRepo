1de36efc3c301cf6ec01b2501b35c18c852c7ae8
davibasa <davi.alves@sou.inteli.edu.br>
2025-06-01 23:17:27 -0300

    dddddddddd

---
+++ davibasa/15 - Reformat Phone Number - 01062242/Solution.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public string ReformatNumber(string number) {
+        // Remove espaços e traços
+        string digits = string.Concat(number.Where(char.IsDigit));
+
+        // Lista para armazenar os blocos formatados
+        List<string> blocks = new List<string>();
+
+        int i = 0;
+        while (digits.Length - i > 4) {
+            blocks.Add(digits.Substring(i, 3));
+            i += 3;
+        }
+
+        // Lida com os últimos 2, 3 ou 4 dígitos
+        int remaining = digits.Length - i;
+        if (remaining == 4) {
+            blocks.Add(digits.Substring(i, 2));
+            blocks.Add(digits.Substring(i + 2, 2));
+        } else {
+            blocks.Add(digits.Substring(i));
+        }
+
+        return string.Join("-", blocks);
+    }
+}
---
+++ davibasa/15 - Reformat Phone Number - 01062242/readme.md
@@ -0,0 +1,18 @@
+## Reformatar Número de Telefone (Reformat Phone Number)
+
+O método `ReformatNumber` formata um número de telefone de acordo com as seguintes regras:
+1. Remove todos os espaços e traços do número.
+2. Agrupa os dígitos da esquerda para a direita em blocos de comprimento 3 até restarem 4 ou menos dígitos.
+3. Os dígitos finais são agrupados da seguinte forma:
+   - 2 dígitos: Um único bloco de comprimento 2.
+   - 3 dígitos: Um único bloco de comprimento 3.
+   - 4 dígitos: Dois blocos de comprimento 2 cada.
+4. Os blocos são unidos por traços.
+
+**Exemplo:**
+- Entrada: "123 4-567"
+- Saída: "123-45-67"
+
+**Complexidade de Tempo:** O(n), onde n é o comprimento do número.
+
+**Complexidade de Espaço:** O(n), para armazenar os blocos formatados.
