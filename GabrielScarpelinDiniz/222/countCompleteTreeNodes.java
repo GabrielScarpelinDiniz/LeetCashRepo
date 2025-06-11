217dc875a7769ff8d32764d7b3cb8645c757878e
GabrielScarpelinDiniz <gabriel.diniz@sou.inteli.edu.br>
2025-06-10 22:33:17 -0300

    feat: problem 222

---
+++ GabrielScarpelinDiniz/222/countCompleteTreeNodes.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int countNodes(TreeNode root) {
+        int countNodes = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return 0;
+
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            countNodes++;
+
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+        return countNodes;
+    }
+}
