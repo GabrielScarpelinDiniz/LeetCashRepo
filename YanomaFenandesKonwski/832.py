56a824b1bb4777a014ae0acc2242ff32ec0e6a62
Yanomafpk <yanoma.konwski@sou.inteli.edu.br>
2025-06-14 19:40:33 -0300

    vamo

---
+++ YanomaFenandesKonwski/832.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        for row in image:
+            esquerda, direita = 0, len(row) - 1
+            while esquerda <= direita:
+                if esquerda == direita:
+                    row[esquerda] = 1 - row[esquerda]
+                else:
+                    # Troca e inverte os bits
+                    row[esquerda], row[direita] = 1 - row[direita], 1 - row[esquerda]
+                esquerda += 1
+                direita -= 1
+        return image
