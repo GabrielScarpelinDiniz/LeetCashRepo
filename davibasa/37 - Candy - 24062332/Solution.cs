825811f4a76dcd8908f60a20b64c3f9d81d815c6
davibasa <davi.alves@sou.inteli.edu.br>
2025-06-25 22:39:07 -0300

    ;-;

--- davibasa/37 - Candy - 24062332/Solution.cs
+++ davibasa/37 - Candy - 24062332/Solution.cs
@@ -1,29 +1,36 @@
 using System;
 using System.Collections.Generic;
 
-public class Solution {
-    public int Candy(int[] ratings) {
+public class Solution
+{
+    public int Candy(int[] ratings)
+    {
         int n = ratings.Length;
         int[] candies = new int[n];
         Array.Fill(candies, 1);
 
         // Left to right pass
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
                 candies[i] = candies[i - 1] + 1;
             }
         }
 
         // Right to left pass
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+            {
                 candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
             }
         }
 
         // Sum up candies
         int totalCandies = 0;
-        foreach (int candy in candies) {
+        foreach (int candy in candies)
+        {
             totalCandies += candy;
         }
 
